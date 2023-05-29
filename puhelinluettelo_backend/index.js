7df5edf37bd8185f272b5951120e77b89b9178c2
Roope <2.r.k.niemi@gmail.com>
2023-05-29 14:17:12 +0300

    index error korjattu

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 var morgan = require('morgan')
-const app = express()
 const cors = require('cors')
 
+const app = express()
 morgan.token('req-body', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
