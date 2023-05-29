c991248b82fb9b1696ccfaf51a7c7b8516cb7417
Roope <2.r.k.niemi@gmail.com>
2023-05-29 13:49:18 +0300

    port change

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -97,7 +97,7 @@ app.get("/info", (req, res) => {
   res.send(txt)
 })
 
-const PORT = 3001
+const PORT = process.env.port || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
